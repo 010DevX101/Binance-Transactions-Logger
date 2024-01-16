433b68d730a89ffe6963edfbc8bb36effe093e0f
010DevX101 <cat.enterprisee@gmail.com>
2024-01-16 12:38:42 -0300

    Removed node_modules from being tracked

--- node_modules/@binance/connector/src/helpers/privateKeyAlgo.js
+++
@@ -1,6 +0,0 @@
-const PrivateKeyAlgo = {
-  RSA: 'RSA',
-  ED25519: 'Ed25519'
-}
-
-module.exports = PrivateKeyAlgo
--- node_modules/@binance/connector/src/modules/websocket/api/index.js
+++
@@ -1,6 +0,0 @@
-'use strict'
-
-module.exports.Market = require('./market')
-module.exports.Account = require('./account')
-module.exports.UserData = require('./userData')
-module.exports.Trade = require('./trade')
